f4e25d50106aa4c526f72d73ff7ea0737079d0ba
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-12 19:13:01 +0500

    update to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import {View, Text, StyleSheet } from 'react-native'
 import React from 'react';
 import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
 
 export default function App(): JSX.Element {
   return (
