f06def9b81f18896c7d45cb65d74a7467454997a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-13 18:12:40 +0500

    add to commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ type DiceProps = PropsWithChildren<{
 const Dice = ({imageUrl}: DiceProps):JSX.Element => {
   return (
     <View>
-      <Image style={styles.diceImages} source={imageUrl} />
+      <Image style={styles.diceImage} source={imageUrl} />
     </View>
   )
 }
@@ -71,4 +71,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FFF2F2',
   },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+rollDiceBtnText: {
+  paddingVertical: 10,
+  paddingHorizontal: 40,
+  borderWidth: 2,
+  borderRadius: 8,
+  borderColor: '#E5E0FF',
+  fontSize: 16,
+  color: '#8EA7E9',
+  fontWeight: '700',
+  textTransform: 'uppercase',
+},
 });
